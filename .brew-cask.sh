781651da97631432d47680fe707fa0daff01be74
Amir Ossanloo <amir@amirossanloo.com>
2020-05-03 21:28:00 -0300

    Update to dotfiles

--- .brew-cask.sh
+++ .brew-cask.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Browsers
+brew cask install brave-browser
 brew cask install google-chrome
 brew cask install firefox
 
@@ -9,13 +10,11 @@ brew cask install skype
 brew cask install slack
 brew cask install whatsapp
 
-# Design
-brew cask install sketch
-
 # Development
 brew cask install docker
 brew cask install postman
 brew cask install sequel-pro
+brew cask install sublime-text
 brew cask install visual-studio-code
 
 # Media
@@ -25,3 +24,8 @@ brew cask install vlc
 # Productivity
 brew cask install 1password
 brew cask install alfred
+
+# Other
+brew cask install adobe-acrobat-reader
+brew cask install sketch
+brew cask install transmit4
